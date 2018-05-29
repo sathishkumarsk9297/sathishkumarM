1a7408e1339288ed6b00cddff75316d4da51992a
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-05-29 12:18:49 +0530

    Hello

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+public static void main(String s[]) {
+System.out.println("Welcome to java");
+byte one=100;
+byte two=15;
+int three=one + two;
+System.out.println(three);
+}
+}
