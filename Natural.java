01e419606d8a1689dce1544f95fdf674058437e1
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-06-22 16:08:43 +0530

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,16 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class Natural {
+public static void main(String[] args) {
+	
+	int a=10,sum=0;
+	
+for(int i=0;i<=a;i++) {
+		sum+=i;
+
+		System.out.println(sum);
+	}
+}
+}
