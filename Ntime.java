3eb302be68e896b7098e029aa4ad92a4d21c4c55
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-06-22 16:03:10 +0530

    Add files via upload

---
+++ Ntime.java
@@ -0,0 +1,15 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class Ntime {
+	public static void main(String[] args) {
+		
+	
+	Scanner sc = new Scanner(System.in);
+	int a=sc.nextInt();
+	sc.close();
+	for(int i=0;i<a;i++) {
+		System.out.println("Sathish");
+	}
+}}
