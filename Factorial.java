81ac0b86e7c30b1aba6b57c299603b95da0838ac
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-06-25 09:47:12 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -6,10 +6,10 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int fact=1;
+		int count=1;
 		int number= sc.nextInt();
 		for(int i=1;i<=number;i++) {
-			fact=fact * i;
+			count=count * i;
 
 		}
 		System.out.println(fact);
