3b3306061bca5a97ce7b1e04dde82333156bf01b
sathishkumarsk9297 <39697329+sathishkumarsk9297@users.noreply.github.com>
2018-06-26 09:42:08 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,25 @@
+package com.guvi;
+
+import java.util.Scanner;
+
+public class Prime {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int flag=0;
+		for(int i=2;i<=n/2;++i) {
+			
+				if(n%i==0) {
+			        flag++;  
+					
+					break;
+				}}
+				if(flag==0) {
+					System.out.println("Prime");
+				}else
+                System.out.println("Not Prime");
+
+				}
+		}
+	
+
